7ac93c44eaa72be40dc2e15591da3b077aa79684
Nilo18 <nikalongurashvili@gmail.com>
2025-09-08 16:11:54 +0400

    Added getMentorById method

--- controllers/mentorsController.js
+++ controllers/mentorsController.js
@@ -6,23 +6,36 @@ async function getMentorsByAmount(req, res, next) {
         const requestedAmount = await Mentor.find().limit(amount)
         res.status(200).json(requestedAmount)
     } catch (err) {
-        res.status(500).send("Couldn't get mentors by amount.")
+        return res.status(500).send("Couldn't get mentors by amount.")
     }
     next()
 }
 
 async function getAllMentors(req, res, next) {
     try {
-        const mentors = Mentor.find({})
+        const mentors = await Mentor.find({})
         res.status(200).json(mentors)
     } catch (err) {
-        res.status(500).send("Couldn't get all of the mentors.")
+        return res.status(500).send("Couldn't get all of the mentors.")
     }
     next()
 }
 
-// async function addMentor
+async function getMentorById(req, res, next) {
+    try {
+        const givenId = req.params.id
+        const match = await Mentor.findById(givenId)
+        if (!match) {
+            return res.status(401).send("Mentor with such an id doesn't exist.")
+        }
+        res.status(200).json(match)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send("Couldn't get mentor by id.")
+    }
+    next()
+}
 
 // async function updateMentor
 
-module.exports = {getMentorsByAmount, getAllMentors}
+module.exports = {getMentorsByAmount, getAllMentors, getMentorById}
--- routes/mentorsRouter.js
+++ routes/mentorsRouter.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mentorsRouter = express.Router()
-const { getMentorsByAmount, getAllMentors } = require('../controllers/mentorsController.js')
+const { getMentorsByAmount, getAllMentors, getMentorById } = require('../controllers/mentorsController.js')
 
 // For getting a specific amount of mentors
 mentorsRouter.get('/amount', getMentorsByAmount)
 
 // For getting every mentor
 mentorsRouter.get('/', getAllMentors)
+
+// For getting by id
+mentorsRouter.get('/:id', getMentorById)
 
 module.exports = mentorsRouter
