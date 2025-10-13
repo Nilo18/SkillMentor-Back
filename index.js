d34915c19bf6a652563fbfab62ff49047a712653
Nilo18 <nikalongurashvili@gmail.com>
2025-10-13 16:05:17 +0400

    Mongo connection test for heroku

--- index.js
+++ index.js
@@ -66,14 +66,13 @@ const port = process.env.PORT || 5000;
 
 // Optional: MongoDB connection test
 // Uncomment if you want to test MongoDB
-/*
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
-*/
 
 // Simple test route
 app.get('/', (req, res) => {
