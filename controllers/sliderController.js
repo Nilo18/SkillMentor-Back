1137e4800e28459b8b776d114126d7baae166f5d
Nilo18 <nikalongurashvili@gmail.com>
2025-12-05 22:43:14 +0400

    Modified url for the slider images

--- controllers/sliderController.js
+++ controllers/sliderController.js
@@ -5,7 +5,7 @@ async function getSlides(req, res, next) {
         const slides = await Slide.find({})
         // We need () here to tell JavaScript to return the object
         const slidesData = slides.map(slide => ({
-            image: `https://seefuture-back-a044db68f5d8.herokuapp.com/slider/${slide.image}`,
+            image: `https://skillmentor-back.onrender.com/slider/${slide.image}`,
             text: slide.text
         }))
         return res.status(200).json(slidesData)
